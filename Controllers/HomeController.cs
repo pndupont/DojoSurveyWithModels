a1be9a687c0f579756906f512f8e9c30be07944d
pndupont <pndupont@gmail.com>
2019-05-15 12:22:30 -0500

    first commit

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using DOJOSURVEYWITHMODEL.Models;
+
+namespace DOJOSURVEYWITHMODEL.Controllers
+{
+    public class HomeController : Controller
+    {
+        [HttpGet("")]
+        public ViewResult Index()
+        {
+            return View("Index");
+        }
+        [HttpPost("result")]
+        public IActionResult Submission(Survey mySurvey)
+        {
+
+            return View("result", mySurvey);
+        }
+
+    }
+}
+        
---
+++ Models/MyModel.cs
@@ -0,0 +1,12 @@
+using System;
+
+namespace DOJOSURVEYWITHMODEL.Models
+{
+    public class Survey
+    {
+        public string Name { get; set; }
+        public string Location { get; set; }
+        public string FavStack { get; set; }
+        public string Comment { get; set; }
+    }
+}
